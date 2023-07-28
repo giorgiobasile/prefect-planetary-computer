7e6fea13f29cb451a38424272f589c356026e42c
Giorgio Basile <giorgiobasile@users.noreply.github.com>
2023-07-28 17:58:52 +0200

    Add PC STAC client configuration

--- prefect_planetary_computer/credentials.py
+++ prefect_planetary_computer/credentials.py
@@ -3,10 +3,13 @@
 from typing import Optional
 
 import dask_gateway
+import planetary_computer
+import pystac_client
 from dask_gateway.auth import JupyterHubAuth
 from prefect.blocks.core import Block
 from pydantic import Field, SecretStr
 
+CATALOG_URL = "https://planetarycomputer.microsoft.com/api/stac/v1/"
 GATEWAY_ADDRESS = (
     "https://pccompute.westeurope.cloudapp.azure.com/compute/services/dask-gateway"
 )
@@ -53,7 +56,7 @@ class PlanetaryComputerCredentials(Block):
         title="JupyterHub API Token",
     )
 
-    def get_gateway_client(self, **gateway_kwargs) -> dask_gateway.Gateway:
+    def get_dask_gateway(self, **gateway_kwargs) -> dask_gateway.Gateway:
         """
         Provides a client for the PC Dask Gateway Server,
         setting the proper addresses and Jupyter authentication.
@@ -68,16 +71,22 @@ class PlanetaryComputerCredentials(Block):
         Example:
             Get a configured Dask Gateway client:
             ```python
-            from prefect_planetary_computer.earthdata import EarthdataCredentials
+            from prefect import flow
+            from prefect_planetary_computer import PlanetaryComputerCredentials
 
-            pc_credentials_block = PlanetaryComputerCredentials(
-                subscription_key = "sub-key",
-                hub_api_token = "hub-token"
-            )
-            gateway_client = pc_credentials_block.get_gateway_client()
+            @flow
+            def example_get_dask_gateway_flow():
+                pc_credentials_block = PlanetaryComputerCredentials(
+                    subscription_key = "sub-key",
+                    hub_api_token = "hub-token"
+                )
+                gateway = pc_credentials_block.get_dask_gateway()
 
-            # List available clusters
-            gateway_client.list_clusters()
+                # List available clusters
+                clusters = gateway.list_clusters()
+                return len(clusters)
+
+            example_get_dask_gateway_flow()
             ```
         """  # noqa E501
         if self.hub_api_token is None:
@@ -88,4 +97,50 @@ class PlanetaryComputerCredentials(Block):
             proxy_address=GATEWAY_PROXY_ADDRESS,
             auth=JupyterHubAuth(api_token=self.hub_api_token.get_secret_value()),
             **gateway_kwargs
+        )
+
+    def get_stac_catalog(self, **pystac_kwargs) -> pystac_client.Client:
+        """
+        Provides a PySTAC client for the PC data catalog, automatically signing items as they are retrieved.
+        For more information about PC signing, refer to the [docs](https://planetarycomputer.microsoft.com/docs/concepts/sas).
+
+        Args:
+            pystac_kwargs: Additional keyword arguments to pass to the [pystac_client.Client.open](https://pystac-client.readthedocs.io/en/stable/api.html#pystac_client.Client.open) method.
+
+        Returns:
+            A PySTAC client for the PC Catalog.
+
+        Example:
+            Get a configured PySTAC client with automatic asset signing:
+            ```python
+            from prefect import flow
+            from prefect_planetary_computer import PlanetaryComputerCredentials
+
+            @flow
+            def example_get_stac_catalog_flow():
+                pc_credentials_block = PlanetaryComputerCredentials(
+                    subscription_key = "sub-key",
+                    hub_api_token = "hub-token"
+                )
+                catalog = pc_credentials_block.get_stac_catalog()
+
+                # Search STAC catalog for Landsat Collection 2 Level 2 items
+                time_range = "2020-12-01/2020-12-31"
+                bbox = [-122.2751, 47.5469, -121.9613, 47.7458]
+
+                search = catalog.search(collections=["landsat-c2-l2"], bbox=bbox, datetime=time_range)
+                items = search.get_all_items()
+                return len(items)
+
+            example_get_stac_catalog_flow()
+            ```
+        """  # noqa E501
+
+        if self.subscription_key:
+            planetary_computer.set_subscription_key(
+                self.subscription_key.get_secret_value()
+            )
+
+        return pystac_client.Client.open(
+            CATALOG_URL, modifier=planetary_computer.sign_inplace, **pystac_kwargs
         )
