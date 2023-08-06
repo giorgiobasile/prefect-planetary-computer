9f61b9f85cfa93b6e670f325ea49556bfab9e5a4
Giorgio Basile <giorgiobasile@users.noreply.github.com>
2023-08-06 18:01:05 +0200

    Share PC constants between modules

---
+++ prefect_planetary_computer/constants.py
@@ -0,0 +1,5 @@
+"""Module providing shared constants for the Planetary Computer integration."""
+
+CATALOG_URL = "https://planetarycomputer.microsoft.com/api/stac/v1/"
+GATEWAY_ADDRESS = "https://pccompute.westeurope.cloudapp.azure.com/compute/services/dask-gateway"  # noqa
+GATEWAY_PROXY_ADDRESS = "gateway://pccompute-dask.westeurope.cloudapp.azure.com:80"
